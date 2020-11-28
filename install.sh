fa50e8f467bcd8e91a328d738a534d7b0dc51dde
Kevin Edwards <kedwards@kevinedwards.ca>
2020-11-28 13:02:20 -0700

    removed VAGRANT_SHELL in favour of PROJECT_PATH

--- install.sh
+++ install.sh
@@ -12,24 +12,14 @@ fi
 
 if [ -z "$1" ]
 then
-  echo "You must provide a project name." 1>&2
+  echo "You must provide a project path." 1>&2
   exit 101
 else
-  PROJECT=$1
+  PROJECT_PATH=$1
 fi
 
-if [ -z "$2" ]
+if [ ! -d ${PROJECT_PATH} ]
 then
-  echo "You must provide a sudo password." 1>&2
-  exit 101
-else
-  SUDO_PASS=$1
-fi
-
-VAGRANT_SHELL=${PROJECT}
-
-if [ ! -d ${VAGRANT_SHELL} ]
-then
-  git clone --depth=1 https://github.com/kedwards/vagrant-shell.git ${VAGRANT_SHELL} && \
-  rm -rf ${VAGRANT_SHELL}/.git\*
+  git clone --depth=1 https://github.com/kedwards/vagrant-shell.git ${PROJECT_PATH} && \
+  rm -rf ${PROJECT_PATH}/.git\*
 fi
